c4e8d8626de3d426bacc8585ac198927bb8b6c77
Mark Barnes <mark@sharonandmark.net>
2018-07-03 14:09:37 +0100

    Only hide menu if WP Admin Bar displaying or device is in landscape mode. Bump version to 1.07.

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@
 
 define( 'CHILD_THEME_NAME', 'Evangelical Magazine Theme' );
 define( 'CHILD_THEME_URL', 'https://www.evangelicalmagazine.com/' );
-define( 'CHILD_THEME_VERSION', '1.06' );
+define( 'CHILD_THEME_VERSION', '1.07' );
 
 //* Start the engine
 include_once (get_template_directory() . '/lib/init.php' );
