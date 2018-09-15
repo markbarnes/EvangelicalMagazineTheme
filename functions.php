42e5ad408a9fadeca783775b626c61c8c57ee13f
Mark Barnes <mark@sharonandmark.net>
2018-09-15 21:10:22 +0100

    Autoversion CSS

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@
 
 define( 'CHILD_THEME_NAME', 'Evangelical Magazine Theme' );
 define( 'CHILD_THEME_URL', 'https://www.evangelicalmagazine.com/' );
-define( 'CHILD_THEME_VERSION', '1.1.0' );
+define( 'CHILD_THEME_VERSION', filemtime (get_stylesheet_directory().'/style.css'));
 
 //* Start the engine
 include_once (get_template_directory() . '/lib/init.php' );
